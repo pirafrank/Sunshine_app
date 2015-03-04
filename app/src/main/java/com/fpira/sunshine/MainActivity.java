2e94583b7d1f3e8effa3ee93e9db53392f8fb9f8
Francesco Pira <francescopira.me@gmail.com>
2015-03-04 20:24:09 +0100

    app bumped to v1.06 - list binded to main activity

--- app/src/main/java/com/fpira/sunshine/MainActivity.java
+++ app/src/main/java/com/fpira/sunshine/MainActivity.java
@@ -51,16 +51,5 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 }
