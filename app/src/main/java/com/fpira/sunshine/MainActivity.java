b48e07f5df9f3baf85c2736bd2892af3a71de70b
Francesco Pira <francescopira.me@gmail.com>
2015-03-13 00:09:53 +0100

    updated to lesson 2.07

--- app/src/main/java/com/fpira/sunshine/MainActivity.java
+++ app/src/main/java/com/fpira/sunshine/MainActivity.java
@@ -1,15 +1,9 @@
 package com.fpira.sunshine;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -20,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
